9e231b8e44d268c01624ccb413f6d739e9588e0b
nampham <nampham18082003@gmail.com>
2023-03-04 15:13:04 +0700

    fe-upadte

--- assets/AdminPage.47268033.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index.ef41b98f.js";const a=()=>e("div",{children:"Admin Page"});export{a as default};
---
+++ assets/PostAdmin.2720f7d9.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index.85bd44a2.js";const r=()=>t("div",{children:"L\xE0 post"});export{r as default};
