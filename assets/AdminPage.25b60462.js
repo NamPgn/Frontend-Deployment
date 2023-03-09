1d514e1b3b8bd6fab41238a00686a451bbadbe20
nampham <nampham18082003@gmail.com>
2023-03-09 22:42:38 +0700

    fe-ud

---
+++ assets/AdminPage.25b60462.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index.2c5c11fb.js";const a=()=>e("div",{children:"Admin Page"});export{a as default};
---
+++ assets/PostAdmin.4fad2a1e.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index.2c5c11fb.js";const r=()=>t("div",{children:"L\xE0 post"});export{r as default};
