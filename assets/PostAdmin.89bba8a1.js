a3a9d9b056af27b8ebf8803ea50dad5308ac6552
nampgn <nampham18082003@gmail.com>
2023-03-31 13:41:07 +0700

    fe-deployment up

---
+++ assets/PostAdmin.89bba8a1.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index.73504b2c.js";const r=()=>t("div",{children:"L\xE0 post"});export{r as default};
---
+++ assets/PostList.245c3d74.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index.73504b2c.js";const r=()=>t("div",{className:"container text-center text-light",children:"Updating..."});export{r as default};
