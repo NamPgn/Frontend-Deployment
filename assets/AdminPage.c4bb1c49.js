a3cef6f098a5ed84196f652a398379d86055e52e
nampgn <nampham18082003@gmail.com>
2023-04-17 20:13:43 +0700

    fe-ud

--- assets/AdminPage.c4bb1c49.js
+++
@@ -1,1 +0,0 @@
-import{s,j as e}from"./index.e2c873a8.js";const t=s.div``,a=()=>e(t,{children:"Admin page"});export{a as default};
---
+++ assets/PostAdmin.15926766.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index.d1cd217e.js";const r=()=>t("div",{children:"L\xE0 post"});export{r as default};
