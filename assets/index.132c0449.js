07f1e6651349462a3197d860c859c08a7121c9ca
nampgn <nampham18082003@gmail.com>
2023-05-16 17:50:12 +0700

    fe-ud

--- assets/index.132c0449.js
+++
@@ -1,1 +0,0 @@
-import{s,j as e}from"./index.c1b0f6e6.js";const t=s.div``,a=()=>e(t,{children:"Admin page"});export{a as default};
---
+++ assets/index.e5cb299b.js
@@ -0,0 +1,1 @@
+import{s,j as e}from"./index.731c63ef.js";const t=s.div``,a=()=>e(t,{children:"Admin page"});export{a as default};
