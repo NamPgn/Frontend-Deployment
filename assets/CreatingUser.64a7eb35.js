c3017ad8baad1fb07d979851444db01bd7eacc06
nampgn <nampham18082003@gmail.com>
2023-05-24 18:26:24 +0700

    fe-ud

---
+++ assets/CreatingUser.64a7eb35.js
@@ -0,0 +1,10 @@
+import{u as l}from"./index.esm.fc993cee.js";import{s,e as c,f as m,j as t,b as u,O as p,Q as d}from"./index.b6ebecb4.js";const h=s.div`
+display: flex;
+justifyContent: center;
+textAlign: center; 
+height: 100vh ;
+`,x=s.button`
+margin: 20px 0;
+`,f=s.form`
+
+`,b=s.input``,S=()=>{const n=c(),a=m(),{handleSubmit:i,register:o}=l();return t(h,{children:u(f,{onSubmit:i(r=>{const e=new FormData;e.append("xlsx",r.xlsx[0]),n(p(e)),a("/admin/users"),d.success("Th\xEAm user th\xE0nh c\xF4ng",{position:"bottom-right"})}),children:[t("label",{className:"form-label",children:"Default file input example"}),t(b,{type:"file",...o("xlsx"),className:"form-control",id:"customFile"}),t(x,{className:"btn btn-success",children:"Submit"})]})})};export{S as default};
---
+++ assets/index.965d6d84.js
@@ -0,0 +1,1 @@
+import{s,j as e}from"./index.b6ebecb4.js";const t=s.div``,a=()=>e(t,{children:"Admin page"});export{a as default};
