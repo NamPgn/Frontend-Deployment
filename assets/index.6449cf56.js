9d7bf5ee36c2d4ccf09222d5678643392c0b0bb8
nampgn <nampham18082003@gmail.com>
2023-08-15 18:51:40 +0700

    fe-ud

---
+++ assets/index.6449cf56.js
@@ -0,0 +1,1 @@
+import{s,j as e}from"./index.6cb976f3.js";const t=s.div``,a=()=>e(t,{children:"Admin page"});export{a as default};
---
+++ assets/index.c96eaf19.js
@@ -0,0 +1,1 @@
+import{C as a}from"./index.esm.9d78f1a0.js";import{b as n,j as r,a0 as d}from"./index.6cb976f3.js";const u=(e,t,l,o)=>n("div",{children:[r("label",{htmlFor:e,children:t}),r(a,{name:e,control:l,rules:o,defaultValue:"",render:({field:s})=>r(d,{...s})})]});export{u as r};
