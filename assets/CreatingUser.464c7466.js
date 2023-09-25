1ee8243319397c705cee2b03d98f8f95a73e30f6
nampgn <nampham18082003@gmail.com>
2023-09-25 13:58:27 +0700

    fe-ud

---
+++ assets/CreatingUser.464c7466.js
@@ -0,0 +1,10 @@
+import{s,g as r,h as c,k as u,j as t,b as m,a$ as h,Q as d}from"./index.fea787ad.js";const p=s.div`
+display: flex;
+justifyContent: center;
+textAlign: center; 
+height: 100vh ;
+`,x=s.button`
+margin: 20px 0;
+`,b=s.form`
+
+`,f=s.input``,A=()=>{const n=r(),a=c(),{handleSubmit:o,register:i}=u();return t(p,{children:m(b,{onSubmit:o(l=>{const e=new FormData;e.append("xlsx",l.xlsx[0]),n(h(e)),a("/dashboard/users"),d.success("Th\xEAm user th\xE0nh c\xF4ng",{position:"bottom-right"})}),children:[t("label",{className:"form-label",children:"Default file input example"}),t(f,{type:"file",...i("xlsx"),className:"form-control",id:"customFile"}),t(x,{className:"btn btn-success",children:"Submit"})]})})};export{A as default};
---
+++ assets/index.e6d54301.js
@@ -0,0 +1,1 @@
+import{s,j as e}from"./index.fea787ad.js";const t=s.div``,a=()=>e(t,{children:"Admin page"});export{a as default};
