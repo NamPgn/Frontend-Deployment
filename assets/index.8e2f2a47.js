2e82afb51426d486c1a7db01dfd1cbe5266e4abc
nampgn <nampham18082003@gmail.com>
2023-10-04 01:03:08 +0700

    fe-ud

---
+++ assets/index.8e2f2a47.js
@@ -0,0 +1,1 @@
+import{s,j as e}from"./index.56e1f4bc.js";const t=s.div``,a=()=>e(t,{children:"Admin page"});export{a as default};
---
+++ assets/type.46523b8f.js
@@ -0,0 +1,1 @@
+import{b7 as o,b8 as a}from"./index.56e1f4bc.js";const t=o(),n=async(e,s)=>a.post(`/type/${e}/${t.user._id}`,s,{headers:{Authorization:`Bearer ${t.token}`}}),i=async(e,s)=>await a.post(`/push/type/category/${e}/${t.user._id}`,s,{headers:{Authorization:`Bearer ${t.token}`}});export{n as d,i as p};
