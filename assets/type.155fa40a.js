f6120a996d36707f4c9bbadc4aa7cee2eee0972a
nampg <nampham18082003@gmail.com>
2024-03-30 11:51:59 +0700

    front-end update ui

--- assets/type.155fa40a.js
+++
@@ -1,1 +0,0 @@
-import{bF as o,bG as a}from"./index.73bf24d3.js";const t=o(),n=async(e,s)=>a.post(`/type/${e}/${t.user._id}`,s,{headers:{Authorization:`Bearer ${t.token}`}}),i=async(e,s)=>await a.post(`/push/type/category/${e}/${t.user._id}`,s,{headers:{Authorization:`Bearer ${t.token}`}});export{n as d,i as p};
