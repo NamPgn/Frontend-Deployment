ea5ecaf6244deb82048ee4a01e306129f1db0c8a
nampg <nampham18082003@gmail.com>
2024-04-08 14:07:17 +0700

    reupdate

--- assets/index.29dcbc58.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index.28c40f8a.js";const t=()=>e("div",{className:"text-[#000]",children:"Admin page"});export{t as default};
---
+++ assets/index.ab96ee4a.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index.a9aa7c35.js";const t=()=>e("div",{className:"text-[#000]",children:"Admin page"});export{t as default};
