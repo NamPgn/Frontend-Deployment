222293147c937818f1af1f2e19fd331231a8f61d
nampgn <nampham18082003@gmail.com>
2024-07-25 14:49:14 +0700

    update Layout Performance...

---
+++ assets/adds.eda8a9b7.js
@@ -0,0 +1,6 @@
+import{s as e,U as r,ah as c,j as s,f as m,aY as u}from"./vendor.66f3a2e0.js";import{e as h,a0 as p}from"./index.f008502a.js";const d=e.div`
+  display: flex;
+  justifycontent: center;
+  textalign: center;
+  height: 100vh;
+`,x=e.input``,y=()=>{const a=h(),n=r(),{handleSubmit:i,register:o}=c();return s(d,{children:m("form",{onSubmit:i(l=>{const t=new FormData;t.append("xlsx",l.xlsx[0]),a(p(t)),n("/dashboard/users"),u.success("Th\xEAm user th\xE0nh c\xF4ng",{position:"bottom-right"})}),children:[s("label",{className:"form-label",children:"Default file input example"}),s(x,{type:"file",...o("xlsx"),className:"form-control",id:"customFile"}),s("button",{className:"btn btn-success mt-3",children:"Submit"})]})})};export{y as default};
