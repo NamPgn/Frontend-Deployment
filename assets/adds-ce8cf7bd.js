a8cfdf643996ced6ecbb79bbbf1047e2dd52157d
nampgn <nampham18082003@gmail.com>
2024-09-20 13:09:59 +0700

    update

---
+++ assets/adds-ce8cf7bd.js
@@ -0,0 +1,6 @@
+import{s as e,O as c,af as m,j as s,aX as u}from"./vendor-96c022cb.js";import{e as p,a2 as x}from"./index-3c5ae90d.js";const d=e.div`
+  display: flex;
+  justifycontent: center;
+  textalign: center;
+  height: 100vh;
+`,h=e.input``,g=()=>{const a=p(),n=c(),{handleSubmit:i,register:o}=m(),r=l=>{const t=new FormData;t.append("xlsx",l.xlsx[0]),a(x(t)),n("/dashboard/users"),u.success("Thêm user thành công",{position:"bottom-right"})};return s.jsx(d,{children:s.jsxs("form",{onSubmit:i(r),children:[s.jsx("label",{className:"form-label",children:"Default file input example"}),s.jsx(h,{type:"file",...o("xlsx"),className:"form-control",id:"customFile"}),s.jsx("button",{className:"btn btn-success mt-3",children:"Submit"})]})})};export{g as default};
