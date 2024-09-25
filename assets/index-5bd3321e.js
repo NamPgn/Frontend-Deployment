5455ddaf486cf6e65c602c5ad2b2dd2449842037
nampgn <nampham18082003@gmail.com>
2024-09-25 20:24:31 +0700

    update respone slug episode movie

---
+++ assets/index-5bd3321e.js
@@ -0,0 +1,1 @@
+import{r as i,j as n,b4 as m}from"./vendor-07012f45.js";const p=i.memo(({totalItems:t,pageSize:r,currentPage:s,onChange:e,className:o,defaultCurrent:a})=>n.jsx(m,{simple:!0,className:o,total:t,current:s,onChange:e,pageSize:r,defaultCurrent:a}));export{p as i};
